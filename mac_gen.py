2faa2ddd43bcbe022ae98036d4c5aeafea7accf5
Olav Morken <olavmrk@gmail.com>
2016-02-07 21:07:28 +0100

    Add different separators.

--- mac_gen.py
+++ mac_gen.py
@@ -3,15 +3,27 @@ from __future__ import print_function
 import argparse
 import random
 
+SEPARATORS = {
+    'colon': ':',
+    'dash': '-',
+    'none': '',
+    'space': ' ',
+}
+
 def parse_args():
     parser = argparse.ArgumentParser(description='Generate random MAC address')
+    parser.add_argument('-s', '--separator',
+                        choices=sorted(SEPARATORS.keys()), default='colon',
+                        help='What separator to use between the bytes in the output.')
     return parser.parse_args()
 
 def main():
     args = parse_args()
+    separator = SEPARATORS[args.separator]
+
     mac = [ random.randint(0, 255) for x in range(0, 6) ]
     mac[0] = (mac[0] & 0xfc) | 0x02
-    mac = ':'.join([ '{0:02x}'.format(x) for x in mac ])
+    mac = separator.join([ '{0:02x}'.format(x) for x in mac ])
     print(mac)
 
 if __name__ == '__main__':
