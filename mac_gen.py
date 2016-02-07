a4c44023adc399875454f414c6ca670afd903893
Olav Morken <olavmrk@gmail.com>
2016-02-07 21:00:53 +0100

    Wrap the code in a main()-function.

--- mac_gen.py
+++ mac_gen.py
@@ -2,7 +2,11 @@
 from __future__ import print_function
 import random
 
-mac = [ random.randint(0, 255) for x in range(0, 6) ]
-mac[0] = (mac[0] & 0xfc) | 0x02
-mac = ':'.join([ '{0:02x}'.format(x) for x in mac ])
-print(mac)
+def main():
+    mac = [ random.randint(0, 255) for x in range(0, 6) ]
+    mac[0] = (mac[0] & 0xfc) | 0x02
+    mac = ':'.join([ '{0:02x}'.format(x) for x in mac ])
+    print(mac)
+
+if __name__ == '__main__':
+    main()
