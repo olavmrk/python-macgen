af2aa50241dff64e00273c6eaf8fdd3946d830bc
Olav Morken <olavmrk@gmail.com>
2016-02-07 21:02:10 +0100

    Add argument parsing.

--- mac_gen.py
+++ mac_gen.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
 from __future__ import print_function
+import argparse
 import random
 
+def parse_args():
+    parser = argparse.ArgumentParser(description='Generate random MAC address')
+    return parser.parse_args()
+
 def main():
+    args = parse_args()
     mac = [ random.randint(0, 255) for x in range(0, 6) ]
     mac[0] = (mac[0] & 0xfc) | 0x02
     mac = ':'.join([ '{0:02x}'.format(x) for x in mac ])
